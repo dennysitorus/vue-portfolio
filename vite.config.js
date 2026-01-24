e5ca4b47a8433acc323a0d25d9d152a678fd22e2
Denny Awang Sitorus <dsitorus@ptadaro.com>
2026-01-24 16:55:14 +0700

    chore: Add staging action workflows

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +16,11 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 })
