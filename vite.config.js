f7977d18f32af8402d3bf188bbad0acfc5cf6d19
Denny Awang Sitorus <dsitorus@ptadaro.com>
2026-01-28 13:50:20 +0700

    Fix: change root dir project on server.

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('Tres') && tag !== 'TresCanvas',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const baseFromEnv = env.VITE_BASE_PATH || "/";
+
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith("Tres") && tag !== "TresCanvas",
+          },
+        },
+      }),
+      tailwindcss(),
+    ],
+    // dev selalu root
+    base: command === "serve" ? "/" : baseFromEnv,
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
         },
       },
-    }),
-    tailwindcss(),
-  ],
-  base: "./",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
     },
-  },
-})
+  };
+});
